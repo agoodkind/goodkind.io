6a4384892dd6f36164602be54d2d7533ff717b1f
Alex Goodkind <1017680+agoodkind@users.noreply.github.com>
2023-11-10 10:25:51 -0800

    First pass of redesign for public

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import About from "@sections/About";
-import Education from "@sections/Education";
+import Section from "@components/Section";
+import Title from "@components/Title";
 import Hero from "@sections/Hero";
 import Information from "@sections/Information";
-import Resume from "@sections/Resume";
 import Skills from "@sections/Skills";
 
 function App() {
   return (
-    <main className="container mx-auto w-full max-w-5xl p-4 sm:max-w-[640px] sm:p-5 md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
-      <div className="grid gap-5 lg:grid-cols-3">
+    <main className="container mx-auto w-full max-w-5xl p-4 sm:max-w-[640px] sm:p-5 md:max-w-3xl">
+      <div className="grid gap-5">
         <div className="space-y-5">
           <Hero />
           <Information />
           <Skills />
-        </div>
-
-        <div className="space-y-5 lg:col-span-2">
-          <About />
-          <Resume />
-          <Education />
+          <Section>
+            <Title>Rest coming soon...</Title>
+          </Section>
         </div>
       </div>
     </main>

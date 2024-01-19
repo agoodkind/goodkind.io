89c23e909b7bfcec528f01917f3a2a2d6a935b02
Alex Goodkind <1017680+agoodkind@users.noreply.github.com>
2024-01-19 11:56:31 -0800

    Delete src/components/Item.tsx
    Signed-off-by: Alex Goodkind <1017680+agoodkind@users.noreply.github.com>

--- src/components/Item.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export default function Item({ children }: { children: ReactNode }) {
-  return <div className="flex items-center space-x-2">{children}</div>;
-}

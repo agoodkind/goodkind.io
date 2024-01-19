d17fb5e32807bad01ee54306d0e3e11432e7f2eb
Alex Goodkind <1017680+agoodkind@users.noreply.github.com>
2024-01-19 11:52:59 -0800

    Minor fixes (#78)
    Signed-off-by: Alex Goodkind <1017680+agoodkind@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import "@styles/main.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./app.tsx";
 
 // todo: https://www.npmjs.com/package/favicons
 

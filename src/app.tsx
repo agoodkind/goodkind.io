80dafab50a6aa2d9d9accbc2b998dc0345929f07
Alex Goodkind <1017680+agoodkind@users.noreply.github.com>
2024-02-17 17:45:18 -0800

    no others

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,15 @@
-import { About } from "@sections/about";
-import { Education } from "@sections/education";
 import { Hero } from "@sections/hero";
 import { Information } from "@sections/information";
-import { Resume } from "@sections/resume";
 import { Skills } from "@sections/skills";
 
 function App() {
   return (
-    <main className="container mx-auto w-full max-w-5xl p-4 sm:max-w-[640px] sm:p-5 md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
-      <div className="grid gap-5 lg:grid-cols-3">
+    <main className="container mx-auto w-full max-w-5xl p-4 sm:max-w-[640px] sm:p-5 md:max-w-3xl">
+      <div className="grid gap-5">
         <div className="space-y-5">
           <Hero />
           <Information />
           <Skills />
-        </div>
-
-        <div className="space-y-5 lg:col-span-2">
-          <About />
-          <Resume />
-          <Education />
         </div>
       </div>
     </main>

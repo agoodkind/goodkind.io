28cca0175afc9e798b114252a024125992115f82
Alex Goodkind <1017680+agoodkind@users.noreply.github.com>
2024-03-06 23:44:47 -0800

    comments

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   clearScreen: false,
   server: {
     port: 3000,
     host: "0.0.0.0",
-    strictPort: true,
-    proxy: {
-      // "/api": {
-      //   target: "http://localhost:5000",
-      //   changeOrigin: true,
-      //   rewrite: (path) => path.replace(/^\/api/, ""),
-      // },
-    }
+    strictPort: true
   }
 });

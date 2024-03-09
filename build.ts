87638257e9c50630548241340b4c32e546f1ea2a
Alex Goodkind <1017680+agoodkind@users.noreply.github.com>
2024-03-08 21:59:24 -0800

    eslint i guess

--- build.ts
+++ build.ts
@@ -1,43 +1,48 @@
 import fs from "node:fs";
 import path from "node:path";
 import postcss from "postcss";
-import { renderToString } from "react-dom/server";
+import { renderToStaticMarkup } from "react-dom/server";
 import postcssConfig from "./postcss.config";
 import { app } from "./src/app";
 
-// input assets
-const inputAssets = path.join(process.cwd(), "assets");
+// inputs
+const assets = path.join(process.cwd(), "src/assets");
+const cssPath = path.join(process.cwd(), "src/styles/main.tailwind.css");
+const staticHtml = renderToStaticMarkup(app());
 
-// Define the output directories
+//  outputs
 const distDir = path.join(process.cwd(), "dist");
 const stylesDir = path.join(distDir, "styles");
 const assetsDir = path.join(distDir, "assets");
 
 // Ensure the output directories exist
+Promise.all([distDir, stylesDir, assetsDir].map((dir) => {
+	return fs.promises.mkdir(dir, { recursive: true });
+}));
+
+
 fs.mkdirSync(distDir, { recursive: true });
 fs.mkdirSync(stylesDir, { recursive: true });
 fs.mkdirSync(assetsDir, { recursive: true });
 
 // Render the app to a string
-const html = renderToString(app());
 
 // Write the rendered app to the output HTML file
 const htmlOutputPath = path.join(distDir, "index.html");
-fs.writeFileSync(htmlOutputPath, html);
+fs.writeFileSync(htmlOutputPath, staticHtml);
 
 // Define the input CSS file path
-const cssInputPath = path.join(process.cwd(), "src/styles/main.tailwind.css");
 
 // Read the input CSS file
-const cssInput = fs.readFileSync(cssInputPath);
+const cssInput = fs.readFileSync(cssPath);
 
 // Process the CSS with PostCSS
 postcss(postcssConfig.plugins)
-  .process(cssInput)
-  .then(({ css }) => {
-    // Write the processed CSS to the output CSS file
-    const cssOutputPath = path.join(stylesDir, "main.css");
-    fs.writeFileSync(cssOutputPath, css);
-  });
+	.process(cssInput)
+	.then(({ css }) => {
+		// Write the processed CSS to the output CSS file
+		const cssOutputPath = path.join(stylesDir, "main.css");
+		fs.writeFileSync(cssOutputPath, css);
+	});
 
-fs.cpSync(inputAssets, assetsDir, { recursive: true });
+fs.cpSync(assets, assetsDir, { recursive: true });
--- src/app.tsx
+++ src/app.tsx
@@ -3,31 +3,29 @@ import { Hero } from "./sections/hero";
 import { Information } from "./sections/information";
 import { Skills } from "./sections/skills";
 
-export const app = function () {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/png" href="/src/assets/src-favico.png" />
-        <title>Alex</title>
-        <link href="styles/main.css" rel="stylesheet" />
-      </head>
-      <body className="bg-gray-100 antialiased">
-        <div id="root">
-          <React.StrictMode>
-            <main className="container mx-auto w-full max-w-5xl p-4 sm:max-w-[640px] sm:p-5 md:max-w-3xl">
-              <div className="grid gap-5">
-                <div className="space-y-5">
-                  <Hero />
-                  <Information />
-                  <Skills />
-                </div>
-              </div>
-            </main>
-          </React.StrictMode>
-        </div>
-      </body>
-    </html>
-  );
+export const app = function (): React.JSX.Element {
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link rel="icon" type="image/png" href="/src/assets/src-favico.png" />
+				<title>Alex</title>
+				<link href="styles/main.css" rel="stylesheet" />
+			</head>
+			<body className="bg-gray-100 antialiased">
+				<React.StrictMode>
+					<main className="container mx-auto w-full max-w-5xl p-4 sm:max-w-[640px] sm:p-5 md:max-w-3xl">
+						<div className="grid gap-5">
+							<div className="space-y-5">
+								<Hero />
+								<Information />
+								<Skills />
+							</div>
+						</div>
+					</main>
+				</React.StrictMode>
+			</body>
+		</html>
+	);
 };

41ee64b67e217f3f5db70f59c473c50c8eda72b8
Alex Goodkind <alex@goodkind.io>
2025-02-17 17:44:53 -0800

    Update eslint.config.ts (#201)
    Signed-off-by: Alex Goodkind <alex@goodkind.io>

--- eslint.config.ts
+++ eslint.config.ts
@@ -18,25 +18,3 @@ export default tseslint.config(
     }
   }
 );
-// export default [
-//   {
-//     files: ["src/*.{js,jsx,ts,tsx}"],
-//     ignores: ["dist/**/*", "chrome-debug-profile"],
-//     plugins: {
-//       react: reactPlugin
-//     },
-//     languageOptions: {
-//       parserOptions: {
-//         ecmaFeatures: {
-//           jsx: true
-//         }
-//       },
-//       globals: {
-//         ...globals.browser
-//       }
-//     },
-//     rules: {
-//       "no-console": [0]
-//     }
-//   }
-// ] satisfies Linter.Config[];

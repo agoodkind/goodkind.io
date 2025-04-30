43cf4020e56c17686c770f34e0eaf6ba716f66a1
Alex Goodkind <alex@goodkind.io>
2025-04-29 21:52:20 -0700

    Add TypeScript configuration files and initial components
    - Created `.prettierrc` for code formatting configuration.
    - Added `tsconfig.app.json` and `tsconfig.node.json` for TypeScript project settings.
    - Introduced `src/types.d.ts` for type definitions.
    - Implemented `BlockTitle` and `LinkButton` components in `src/components`.
    - Established a layout component in `src/pages/Layout.tsx` and a configuration file in `src/pages/+config.ts`.
    - Included main CSS styles in `src/styles/main.css` for styling the application.

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,23 @@
+import "@styles/main.css";
+
+export default function PageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div
+      className={
+        " bg-gray-100 flex h-[100vh] w-full antialiased dark:bg-neutral-900"
+      }
+    >
+      <main
+        className={"container mx-auto w-full max-w-xl p-4 sm:max-w-3xl sm:p-5"}
+      >
+        <div className={"grid gap-5"}>
+          <div className={"space-y-5"}>{children}</div>
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ src/types.d.ts
@@ -0,0 +1,16 @@
+import type { PageContext } from "vike/types";
+
+export type TPageContext = {
+  Page: () => JSX.Element;
+  // this is some really shitty type work
+  // wtf
+  // vike really didn't even try here
+  data: Record<string, unknown> & {
+    title?: string;
+  };
+  config: Record<string, unknown> & {
+    title?: string;
+  };
+  isHydration: boolean;
+  enableEagerStreaming: boolean;
+} & PageContext;
